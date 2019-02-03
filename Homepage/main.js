62a6816420d1d3dd2b41ee22a137f6b01c140d7d
Qintu <tqt010114@icloud.com>
2019-02-03 14:32:21 -0800

    toggle effect on the "+" Icon
    The + sign will turn into a - sign after the button is clicked

--- Homepage/main.js
+++ Homepage/main.js
@@ -2,21 +2,20 @@ $(document).ready(function() {
   // initializing values
   let $menu = $(".appIcon");
   let $apps = $(".appLink");
+
   // initializing the searchBar
   $("#searchBar").on("keydown", function(event){
     if(event.which == "13"){
       event.preventDefault();
-      const forwardSlashChecker = /\\/;
-      const inputField = forwardSlashChecker.test($("#searchBar").val()) ? "" :  $("#searchBar").val();
-      window.open(`https://duckduckgo.com/?q=${inputField}`);
+      const forwardSlashChecker = /\\/; //prevent forward slash
+      const inputField = forwardSlashChecker.test($("#searchBar").val()) ? "" :  $("#searchBar").val(); //search duck-duck-go
+      window.open(`https://duckduckgo.com/?q=${inputField}`);//opens an new window
     }
   });
 
-
-
+  //fadeIn/Out toggle for app icons
   $menu.click(function(event) {
-    $apps.toggle("fast", function() {
-
-    })
+    $apps.toggle("fast");
+    $(".Line2").toggle("fast");
   })
 });
